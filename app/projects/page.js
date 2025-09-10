be241a3248542f3c7ea78da98bb3775f270d05d8
Anwarhacker <patelanwar647@gmail.com>
2025-09-10 19:32:22 +0530

    increase border sharpness & all project tetx-black

--- app/projects/page.js
+++ app/projects/page.js
@@ -92,7 +92,7 @@ export default function ProjectsPage() {
   const techStacks = [...new Set(projects.flatMap((p) => p.techStack))];
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 text-black">
       <Navbar />
       <main className="pt-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
