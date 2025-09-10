2f3ba52c43a6bb1f7a0597fdc5ee02dae1a7d649
Anwarhacker <patelanwar647@gmail.com>
2025-09-10 13:07:20 +0530

    added mango atlas

--- app/admin/new/page.js
+++ app/admin/new/page.js
@@ -78,7 +78,7 @@ export default function NewProjectPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 text-black">
       <Navbar />
       <main className="pt-16">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
